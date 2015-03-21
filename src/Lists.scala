eae2abd1e0bd6463a2eee50246730543cb789e4a
Aditya Joshi <1adityajoshi@gmail.com>
2015-03-21 11:06:40 +0530

    Completed a few more problems and added the Trees class.

--- src/Lists.scala
+++ src/Lists.scala
@@ -53,7 +53,70 @@ class Lists
     }
     
     
+    def isPalindrome(xs : List[Int]) : Boolean = {
+      
+        xs == reverse(xs)
+      
+    }
     
+    def flatten(xs : List[Any]) : List[Any] = {
+      
+      if(xs.length == 0) xs      
+      else {
+      xs.head match {
+          case _: Int => xs.head :: flatten(xs.tail)
+          case a : List[Any] => flatten(a) ++ flatten(xs.tail)
+          
+        }
+      }
+      
+    }
+    
+    def compress(xs : List[Character]) : List[Character] = {
+        
+        def compressHelper(xs : List[Character], last : Character) : List[Character] = {
+              
+              if(xs.length == 0) List(last)
+              else if(xs.head == last) compressHelper(xs.tail, last)
+              else last :: compressHelper(xs.tail, xs.head)
+          
+        }
+      compressHelper(xs, 'z').tail
+      
+    }
+    
+    def pack(xs : List[Char]) : List[List[Char]] = {
+      
+        def generate(x : Char, n : Int) : List[Char] = {
+            if(n == 0) List()
+            else x :: generate(x, n - 1)
+        }
+        
+        def packHelper(xs : List[Char], last : Char, num : Int) : List[List[Char]] = {
+          
+          if(xs.length == 0) Nil
+          else {
+              if(xs.head == last) packHelper(xs.tail, last, num + 1)
+              else generate(last, num) :: packHelper(xs.tail, xs.head, 1)
+          }
+        }
+        packHelper(xs.tail, xs.head, 1)      
+    }
+    
+    def encode(xs : List[Char]) : List[(Int, Char)] = {
+      
+        def encodeHelper(xs : List[Char], last : Char, n : Int) : List[(Int, Char)] = {
+          
+            if(xs.length == 0) Nil
+            else {
+                if(xs.head == last) encodeHelper(xs.tail, last, n + 1)
+                else (n, last) :: encodeHelper(xs.tail, xs.head, 1)
+              
+            }
+          
+        }
+      encodeHelper(xs.tail, xs.head, 1)
+    }
     
     
 
--- src/Main.scala
+++ src/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args : Array[String]) : Unit = {
       val lists = new Lists()
       
-      println(lists.nth(1, List(1, 2, 3, 4, 5)))
+      println(lists.encode(List('a', 'a', 'a', 'b', 'b', 'c', 'd')))
     
   }
 }
