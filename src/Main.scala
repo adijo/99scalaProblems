8936070dc68c9418e7a63a728b619265289fd261
Aditya Joshi <1adityajoshi@gmail.com>
2015-03-22 16:48:13 +0530

    Added tree solutions.

--- src/Main.scala
+++ src/Main.scala
@@ -2,9 +2,7 @@
 object Main {
  
   def main(args : Array[String]) : Unit = {
-      val logic = new Logic()
-      val huffVals = List(('a', 45), ('b', 13), ('c', 12), ('d', 16), ('e', 9), ('f', 5))
-      val huffCodings = logic.huffman(huffVals)
-      println(huffCodings)
+      val t = new Trees()
+      println(t.test())
   }
 }
--- src/Trees.scala
+++ src/Trees.scala
@@ -1,3 +1,41 @@
 class Trees {
+  
+  sealed abstract class Tree[+T]
+  
+  case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+    override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+    def getLeft() : Tree[T] = left
+    def getRight() : Tree[T] = right
+    def isLeaf() : Boolean = (left == End && right == End)
+  }
+  case object End extends Tree[Nothing] {
+    override def toString = "."
+  }
 
+  
+  def leafCount[T](tree : Tree[T]) : Int = {
+      
+      tree match {
+        case End => 0
+        case node : Node[T] => 
+          if(node.isLeaf()) 1
+          else leafCount(node.getLeft()) + leafCount(node.getRight())
+        
+      }
+  }
+  
+  def leafList[T](tree : Tree[T]) : List[Tree[T]] = {
+    
+     tree match {
+       case End => List()
+       case node : Node[T] =>
+         if(node.isLeaf()) List(node)
+         else leafList(node.getLeft()) ++ leafList(node.getRight())
+       
+     }
+    
+  }
+  
+  
+  
 }
