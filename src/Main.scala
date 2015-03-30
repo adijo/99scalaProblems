0afbe27cbacc69c9bf1a45481e25ee1b955c555d
Aditya Joshi <1adityajoshi@gmail.com>
2015-03-30 22:21:05 +0530

    Added class for Multiway Trees, added code for counting nodes in a multiway tree.

--- src/Main.scala
+++ src/Main.scala
@@ -2,10 +2,7 @@
 object Main {
  
   def main(args : Array[String]) : Unit = {
-      val lists = new Lists()
-      val list = List(1, 2, 3, 4, 5, 6, 7)
-      println(lists.split(list, 7))
-    
+      val lists = new MultiwayTrees()
       
   }
 }
